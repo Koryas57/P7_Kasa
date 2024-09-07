43135eb7a76c4f596f55634ef5b3c9730b92edd7
Yass <ynezreg@gmail.com>
2024-09-07 12:16:16 +0200

    style(components): add responsive design for components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import './Footer.scss';
+import logo from '../assets/LOGO.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2020 Kasa. All rights reserved</p>
+        <img src={logo} alt="Logo textuel Kasa avec une maison à la place de la première lettre A" />
+        <p>&copy; 2020 Kasa. All rights reserved</p>
     </footer>
   );
 };
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom"
+import Layout from "../components/Layout"
 
 export const NotFoundPage = () => {
   return (
+      <Layout>
+
       <Link to="/">
       <h1>404</h1>
       <h2>Oups! La page que vous demandez n'existe pas.</h2>
       </Link>
 
+      </Layout>
   )
 }
 
