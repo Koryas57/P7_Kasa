e83ba25510263c83c92157701d9fb09bc809ffff
Yass <ynezreg@gmail.com>
2024-09-19 12:39:22 +0200

    style: improve styling of NotFoundPage

--- src/pages/error/NotFoundPage.tsx
+++ src/pages/error/NotFoundPage.tsx
@@ -8,7 +8,7 @@ export const NotFoundPage:React.FC = () => {
       <h1>404</h1>
       <h2>Oups! La page que vous demandez n'existe pas.</h2>
       <NavLink to="/">
-      <p>Retourner sur la page d’accueil</p>
+      <p className="return-link">Retourner sur la page d’accueil</p>
       </NavLink>
     </>
   )
